22940fe5969c163643517ca634fcf6e5d608dc07
Budi Gunawan <gunawan830@gmail.com>
2019-09-08 18:47:22 +0700

    Update _headerContent.php

--- application/views/_layout/_headerContent.php
+++ application/views/_layout/_headerContent.php
@@ -3,31 +3,5 @@
 	  <?php echo @$judul; ?>
 	  <small><?php echo @$deskripsi; ?></small>
 	</h1>
-	<div class="breadcrumb-line">
-	<ul class="breadcrumb">
-	  <?php
-	  	for ($i=0; $i<count($this->session->flashdata('segment')); $i++) { 
-	  		if ($i == 0) {
-	  		?>
-				<li><i class="fa fa-dashboard"></i> <?php echo $this->session->flashdata('segment')[$i]; ?></li>
-	  		<?php
-	  		} elseif ($i == (count($this->session->flashdata('segment'))-1)) {
-  			?>
-				<li class="active"> <?php echo $this->session->flashdata('segment')[$i]; ?> </li>
-	  		<?php
-	  		} else {
-  			?>
-				<li> <?php echo $this->session->flashdata('segment')[$i]; ?> </li>
-	  		<?php
-	  		}
-
-	  		if ($i == 0 && $i == (count($this->session->flashdata('segment'))-1)) {
-	  		?>
-				<li class="active"> Disini </li>
-	  		<?php
-	  		}
-	  	}
-	  ?>
-	</ol>
-	</div>
+	
 </section>
