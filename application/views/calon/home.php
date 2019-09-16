f3fd9e157ff58e875bfbd67efc24b9a837da7596
Budi Gunawan <gunawan830@gmail.com>
2019-09-17 01:43:01 +0700

    Update home.php

--- application/views/calon/home.php
+++ application/views/calon/home.php
@@ -406,7 +406,11 @@ $(document).ready(function() {
                 "orderable": false, //set not orderable
             },
         ],
-
+        aLengthMenu: [
+                [10, 25, 50, 100, 200, -1],
+                [10, 25, 50, 100, 200, "All"]
+            ],
+            iDisplayLength: 10
     });
 
     //datepicker
